5f790a80911ba9c6de7e647058a7e02750324f02
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-29 00:59:50 -0400

    version bump

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
-  summary: "[FIXED] Mandrill: Send templated email and more via Mandrill",
-  name: 'semeano:mandrill',
-  version: '1.0.2',
-  git: "https://github.com/semeano/meteor-mandrill",
-  documentation: 'README.md'
+	summary: '[FIXED] Mandrill: Send templated email and more via Mandrill',
+	name: 'semeano:mandrill',
+	version: '1.0.3',
+	git: 'https://github.com/semeano/meteor-mandrill',
+	documentation: 'README.md',
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.0');
-  api.use(['email', 'http'], ['server']);
-  api.addFiles('mandrill.js', 'server');
-  api.export('Mandrill');
+	api.versionsFrom('METEOR@1.8.1');
+	api.use(['email', 'http'], ['server']);
+	api.addFiles('mandrill.js', 'server');
+	api.export('Mandrill');
 });
 
 Package.onTest(function (api) {
-  api.use('tinytest');
-  api.use('wylio:mandrill');
-  api.addFiles('tests.js', 'server');
+	api.use('tinytest');
+	api.use('wylio:mandrill');
+	api.addFiles('tests.js', 'server');
 });
