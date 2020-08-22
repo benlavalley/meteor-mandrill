94eee3ca1a57ee2e7c621d446552c017375342a0
Ben Lavalley <ben@nibbl.com>
2020-08-22 13:22:58 -0400

    update to be compatible with meteor 1.11 - introduces new email package that possibly breaks compatibility

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
 	summary: '[FIXED] Mandrill: Send templated email and more via Mandrill',
 	name: 'semeano:mandrill',
-	version: '1.0.3',
+	version: '1.0.4',
 	git: 'https://github.com/semeano/meteor-mandrill',
 	documentation: 'README.md',
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('METEOR@1.8.1');
+	api.versionsFrom('METEOR@1.11');
 	api.use(['email', 'http'], ['server']);
 	api.addFiles('mandrill.js', 'server');
 	api.export('Mandrill');
@@ -15,6 +15,6 @@ Package.onUse(function (api) {
 
 Package.onTest(function (api) {
 	api.use('tinytest');
-	api.use('wylio:mandrill');
+	api.use('semeano:mandrill');
 	api.addFiles('tests.js', 'server');
 });
